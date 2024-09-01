7377de20546ed0341157e285b105e376f00226b8
Peanuts-83 <tranque@free.fr>
2024-09-01 21:21:43 +0200

    FK fix

--- src/app/models/booking.interface.ts
+++ src/app/models/booking.interface.ts
@@ -1,10 +1,10 @@
 export interface BookingList {
-  booking_id: number
-  ref_guest_id: string
+  id: number
+  guest_id: string
   check_in_date: Date
   check_out_date: Date
   num_guests: number
   is_checked_in: boolean
-  ref_room_id: number
-  ref_invoice_id: number
+  room_id: number
+  invoice_id: number
 }
